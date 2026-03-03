7ea80419a8354aacc67d7a42383e87bde8e064bd
Irina Gayntdinova <gajntdinovairina@gmail.com>
2026-03-03 19:39:53 +0500

    1

--- index.js
+++ index.js
@@ -16,10 +16,23 @@ function processCommand(command) {
         case 'exit':
             process.exit(0);
             break;
+        case 'show':
+            parseFile(files);
+            break;
         default:
             console.log('wrong command');
             break;
     }
 }
 
-// TODO you can do it!
+function parseFile(file) {
+    for (let file of files) {
+        for (let str of file.split('\n')) {
+            if (str.includes('// TODO')){
+                const processedStr = str.split('// TODO ')[1];
+                if (processedStr !== undefined)
+                    console.log(processedStr);
+            }
+        }
+    }
+}
