47dc56ee836f379525946b6aeaac5e2835b08008
Irina Gayntdinova <gajntdinovairina@gmail.com>
2026-03-03 20:36:40 +0500

    5

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ function processCommand(command) {
         case 'sort':
             sortFile(command.split(" ")[1]);
             break;
-
         default:
             console.log('wrong command');
             break;
@@ -70,36 +69,124 @@ function userNameFile (userName) {
         }
     }
 }
+
 function sortImportance(){
-    const Arr= []
+    const importantTodos = [];
+    const normalTodos = [];
+
     for (let file of files) {
         for (let str of file.split('\n')) {
             if (str.includes('// TODO')){
                 const processedStr = str.split('// TODO ')[1];
-                if (str.includes('!')){
-                    if (processedStr !== undefined)
-                        console.log(processedStr);
+                if (processedStr !== undefined) {
+                    const exclamationCount = (processedStr.match(/!/g) || []).length;
+
+                    if (exclamationCount > 0) {
+                        importantTodos.push({text: processedStr, count: exclamationCount});
+                    } else {
+                        normalTodos.push(processedStr);
+                    }
                 }
-                else {
-                    if (processedStr !== undefined)
-                        Arr.push(processedStr);
-                }
-            
             }
         }
     }
-    for (let element of Arr){
-        console.log(element);
+
+    importantTodos.sort((a, b) => b.count - a.count);
+    for (let todo of importantTodos) {
+        console.log(todo.text);
+    }
+    for (let todo of normalTodos) {
+        console.log(todo);
     }
 }
 
-function sortFile (sortBy) {
+function sortUser(){
+    const userTodos = {};
+    const anonymousTodos = [];
+
+    for (let file of files) {
+        for (let str of file.split('\n')) {
+            if (str.includes('// TODO')){
+                const processedStr = str.split('// TODO ')[1];
+                if (processedStr !== undefined) {
+                    const parts = processedStr.split(';');
+                    const userName = parts[0].trim();
+                    if (userName && parts.length >= 1) {
+                        if (!userTodos[userName]) {
+                            userTodos[userName] = [];
+                        }
+                        userTodos[userName].push(processedStr);
+                    } else {
+                        anonymousTodos.push(processedStr);
+                    }
+                }
+            }
+        }
+    }
+
+    const sortedUsers = Object.keys(userTodos).sort();
+
+    for (let user of sortedUsers) {
+        console.log(user + ':');
+        for (let todo of userTodos[user]) {
+            console.log('  ' + todo);
+        }
+    }
+
+    if (anonymousTodos.length > 0) {
+        for (let todo of anonymousTodos) {
+            console.log('  ' + todo);
+        }
+    }
+}
+function sortDate(){
+    const withDate = [];
+    const withoutDate = [];
+
+    for (let file of files) {
+        for (let str of file.split('\n')) {
+            if (str.includes('// TODO')){
+                const processedStr = str.split('// TODO ')[1];
+                if (processedStr !== undefined) {
+                    const parts = processedStr.split(';');
+
+                    if (parts.length >= 2) {
+                        const dateStr = parts[1].trim();
+                        const date = new Date(dateStr);
+
+                        if (!isNaN(date.getTime())) {
+                            withDate.push({text: processedStr, date: date});
+                        } else {
+                            withoutDate.push(processedStr);
+                        }
+                    } else {
+                        withoutDate.push(processedStr);
+                    }
+                }
+            }
+        }
+    }
+    withDate.sort((a, b) => b.date - a.date);
+    for (let todo of withDate) {
+        console.log(todo.text);
+    }
+    for (let todo of withoutDate) {
+        console.log(todo);
+    }
+}
+function sortFile(sortBy) {
     switch (sortBy){
         case 'importance':
-           sortImportance();
-           break; 
+            sortImportance();
+            break;
+        case 'user':
+            sortUser();
+            break;
         case 'date':
-            
+            sortDate();
+            break;
+        default:
+            console.log('Unknown sort type. Use: importance, user, or date');
+            break;
     }
-    
 }
